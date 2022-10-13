25a1c72eda6340db21be8bae561acecee7d5b845
jhvgpa <85534766+jhvgpa@users.noreply.github.com>
2022-10-13 21:52:45 +0800

    Add files via upload

---
+++ GUINTO_ITE6102-LAB_ACT1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << R"(
+        
+88  dP    db    88 .dP"Y8 88888888 88888888 88888888 
+88odP    dPYb   88 `Ybo." 88____   88____      88   
+88"Yb   dP__Yb  88 o.`Y8b 88///    88///       88   
+88  Yb dP""""Yb 88 8bodP' 88888888 88          88   
+ 
+    )";
+    return 0;
+}
